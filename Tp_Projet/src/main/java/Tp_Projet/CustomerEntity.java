43db36c429ebecce8022b25160fef479f07a6fde
Yasmina <Yasmina@LAPTOP-K726GKET>
2019-03-26 14:01:36 +0100

    dao customer

---
+++ Tp_Projet/src/main/java/Tp_Projet/CustomerEntity.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tp_Projet;
+
+/**
+ *
+ * @author Yasmina
+ */
+public class CustomerEntity {
+    // TODO : ajouter les autres propriétés
+	private int customerId;
+	private String name;
+	private String email;
+
+	public CustomerEntity(int customerId, String name, String email) {
+		this.customerId = customerId;
+		this.name = name;
+		this.email = email;
+	}
+
+	/**
+	 * Get the value of customerId
+	 *
+	 * @return the value of customerId
+	 */
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	/**
+	 * Get the value of name
+	 *
+	 * @return the value of name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Get the value of addressLine1
+	 *
+	 * @return the value of addressLine1
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+
+}
